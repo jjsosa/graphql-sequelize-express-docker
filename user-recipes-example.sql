a5bd98635794edb65dcf7bdfbb36c9e818199c32
Juan José Sosa Osuna <sosaosuna@gmail.com>
2019-12-19 12:40:45 +0100

    .sql script modified

---
+++ user-recipes-example.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE graphqlexample;
+
+CREATE TABLE `user` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`email` varchar(50) NOT NULL,
+	`password` text NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `unique-email-user` (`email`)
+);
+
+CREATE TABLE `recipe` (
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`title` varchar(50) NOT NULL,
+	`ingredients` text DEFAULT NULL,
+	`direction` text DEFAULT NULL,
+	`userId` bigint(20) unsigned NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `unique-title-recipe` (`title`),
+	KEY `fk-user-recipe` (`userId`),
+	CONSTRAINT `fk-constr-user-recipe` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+);
